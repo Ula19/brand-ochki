ef296a896315cee9ded93b37ef10ec5897bb88b8
Ula19 <ulug.2000.2@gmail.com>
2026-06-29 15:18:33 +0500

    добавил FSM-состояния для админки

---
+++ app/states/admin.py
@@ -0,0 +1,35 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class AddCategory(StatesGroup):
+    waiting_name = State()
+
+
+class AddBrand(StatesGroup):
+    waiting_name = State()
+
+
+class AddProduct(StatesGroup):
+    waiting_name = State()
+    waiting_description = State()
+    waiting_currency = State()
+    waiting_price = State()
+    waiting_stock = State()
+    waiting_brand = State()
+    waiting_new_brand_name = State()
+    waiting_category = State()
+    waiting_new_category_name = State()
+    waiting_photo = State()
+    confirm = State()
+
+
+class EditProductPrice(StatesGroup):
+    waiting_value = State()
+
+
+class EditProductStock(StatesGroup):
+    waiting_value = State()
+
+
+class EditSetting(StatesGroup):
+    waiting_value = State()
