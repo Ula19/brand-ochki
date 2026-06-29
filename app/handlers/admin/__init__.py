01405db1dda51d71c83e59397f657e0233ec83b8
Ula19 <ulug.2000.2@gmail.com>
2026-06-29 15:18:33 +0500

    собрал роутер админки с фильтром доступа

---
+++ app/handlers/admin/__init__.py
@@ -0,0 +1,13 @@
+from aiogram import Router
+
+from app.filters.admin import IsAdmin
+from app.handlers.admin import brands, categories, menu, products, texts
+
+admin_router = Router()
+admin_router.message.filter(IsAdmin())
+admin_router.callback_query.filter(IsAdmin())
+admin_router.include_router(menu.router)
+admin_router.include_router(categories.router)
+admin_router.include_router(brands.router)
+admin_router.include_router(products.router)
+admin_router.include_router(texts.router)
